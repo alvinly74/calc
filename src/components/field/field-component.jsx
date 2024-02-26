682c478b2f4e41f6e4655cf5a056cba9bf3ebc2e
alvin ly <alvinly74@gmail.com>
2024-02-26 14:40:38 -0800

    add enter button

--- src/components/field/field-component.jsx
+++ src/components/field/field-component.jsx
@@ -2,8 +2,11 @@ import React, { useState } from 'react';
 import NumberButton from '../numberButton/numberButton-component'
 import OperationButton from '../operationButton/operationButton-component'
 import ClearButton from '../clearButton/clearButton-component'
+import SubmitButton from '../submitButton/submitButton-component'
 const Field = () => {
   const [currentValue, setCurrentValue] = useState('');
+  const [calculatedValue, setCalculatedValue] = useState(''); // Add this line
+
 
   const updateField = (value) => {
     setCurrentValue(currentValue + value);
@@ -12,6 +15,35 @@ const Field = () => {
   const clearField = () => {
     setCurrentValue('');
   };
+
+  const submit = async() => {
+    // perform pemdas on input
+    // setCalculatedValue(4);
+    console.log(currentValue)
+    const data = {currentValue}
+    try {
+      const response = await fetch('https://zvcz2xe5bwzfzdozd2mxazfcfu0mcxad.lambda-url.us-east-1.on.aws/', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(data),
+      });
+  
+      if (!response.ok) {
+        throw new Error(`Error: ${response.status}`);
+      }
+  
+      const result = response.json();
+      // Update your UI with the received result (e.g., display it in a field)
+      setCalculatedValue(currentValue)
+    } catch (error) {
+      console.error('Error:', error);
+      // Handle errors, like displaying an error message to the user
+    }
+
+    
+  };
+
+  
 
   return (
     <div>
@@ -31,7 +63,9 @@ const Field = () => {
       <NumberButton number={0} onClick={updateField} />
       <OperationButton operation={"/"} onClick={updateField} />
       <ClearButton onClick={clearField}/> 
-      {/* Add more number buttons for other digits */}
+      <SubmitButton onClick={submit}/>
+      <input type="text" value={calculatedValue} readOnly />
+
     </div>
   );
 };
